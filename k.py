38b0d57acdf384a6b8f4a499585e03130aa6b90e
GutuAlexei <70639790+GutuAlexei@users.noreply.github.com>
2021-01-28 12:59:56 +0300

    Add files via upload

---
+++ k.py
@@ -0,0 +1,6 @@
+prenume=['Alexei' , 'Andrei' , 'Ion' , 'Eugen' , 'Andreea']
+varsta=[14 , 18 , 12 , 7 , 3]
+for i in range(0 , len(prenume)):
+    print(prenume[i] , 'are varsta de ' , varsta[i] , 'ani')
+    i+=1
+print()
