b0e8784745bcfb867ed1bb66f9cce70bc4c0da65
GutuAlexei <70639790+GutuAlexei@users.noreply.github.com>
2021-01-31 22:42:04 +0200

    Delete k.py

--- k.py
+++
@@ -1,6 +0,0 @@
-prenume=['Alexei' , 'Andrei' , 'Ion' , 'Eugen' , 'Andreea']
-varsta=[14 , 18 , 12 , 7 , 3]
-for i in range(0 , len(prenume)):
-    print(prenume[i] , 'are varsta de ' , varsta[i] , 'ani')
-    i+=1
-print()
